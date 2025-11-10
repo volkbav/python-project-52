0eb2eebee20cc33cc087119f6c179c7cd4979b31
alex <berezov86@gmail.com>
2025-11-10 15:59:47 +0300

    add translate

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -135,6 +135,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [BASE_DIR / 'locale']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,12 +1,12 @@
 # task_manager/views.py
 from django.views.generic import TemplateView
-
+from django.utils.translation import gettext as _
 
 class HomePageView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'root.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['who'] = 'World'
+        context['who'] = _('World')
         return context
 
