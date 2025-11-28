39eaeb5bf6f58f9c23ed4f7a075572b8d7a34231
alex <berezov86@gmail.com>
2025-11-28 11:54:48 +0300

    fix lint

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.contrib.auth.models import User
+
+# from django.contrib.auth.models import User
 
 
 def index(request):
