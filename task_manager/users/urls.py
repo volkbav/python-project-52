9768e28e8ab6586279844b3a33c2a6ff94d5f966
volkbav <berezov86@gmail.com>
2025-11-29 22:45:49 +0300

    fix lint

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from task_manager.users.views import UsersIndexView, UserFormCreateView
 
+from task_manager.users.views import UserFormCreateView, UsersIndexView
 
 urlpatterns = [
     path('', UsersIndexView.as_view(), name='users'),
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
 
 # Create your views here.
 # from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic import ListView
+
 from .forms import UserFormCreate
 
 
@@ -14,6 +15,7 @@ class UsersIndexView(ListView):
     template_name = "users/index.html"
     context_object_name = "users"
 
+
 # path 'create/
 class UserFormCreateView(View):
     def get(self, request, *args, **kwargs):
@@ -22,8 +24,9 @@ class UserFormCreateView(View):
         
     def post(self, request, *args, **kwargs):
         form = UserFormCreate(request.POST)
-        if form.is_valid(): # Если данные корректные, то сохраняем данные формы
+        if form.is_valid():  # Если данные корректные, то сохраняем данные формы
             form.save()
-            return redirect('users') # Редирект на указанный маршрут
-        # Если данные некорректные, то возвращаем человека обратно на страницу с заполненной формой
+            return redirect('users')  # Редирект на указанный маршрут
+        # Если данные некорректные, то возвращаем человека обратно 
+        # на страницу с заполненной формой
         return render(request, 'users/create.html', {'form': form})
