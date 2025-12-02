0bfddfa009463e70578c77f7cf3291fd027a778f
alex <berezov86@gmail.com>
2025-12-02 16:47:37 +0300

    add app_name = "users"

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from task_manager.users.views import UserFormCreateView, UsersIndexView
 
+app_name = 'users'
 urlpatterns = [
     path('', UsersIndexView.as_view(), name='users'),
     path("create/", UserFormCreateView.as_view(), name="user_create"),
