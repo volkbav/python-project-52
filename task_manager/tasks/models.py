59b1b5ee09d91912e561ca69c1a8b973d3d3e9dd
alex <berezov86@gmail.com>
2025-12-05 16:05:17 +0300

    step5

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from task_manager.statuses.models import Status
+
 
 
 # Create your models here.
 class Task(models.Model):
     name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        blank=False,
+        related_name='tasks',
+#        null=True
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='users',
+#        null=True
+    )
     
  #   class Meta:
  #       verbose_name_plural = 'tasks'
