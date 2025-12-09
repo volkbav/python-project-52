95c37a5667287c29a42721921731451e7b482741
alex <berezov86@gmail.com>
2025-12-09 14:50:07 +0300

    add CRUD tests for tasks

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,79 +1,95 @@
-# from django.test import TestCase
+# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
 
-# # Create your tests here.
-# from django.contrib.auth.models import User
-# from django.urls import reverse
+from task_manager.statuses.models import Status
 
-# from .models import Status
+from .models import Task
 
 
-# class UserTest(TestCase):
-#     def setUp(self):
-#         self.status = Status.objects.create(
-#             name="status for tests"            
-#         )
-#         self.user = User.objects.create_user(
-#             username="Jonny",
-#             password="password123"  # NOSONAR
-#         )
-#         # входим в приложение под пользователем.
-#         self.client.login(username="Jonny", password="password123")  # NOSONAR
+class TaskTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="Jonny",
+            password="password123"  # NOSONAR
+        )
+        self.task = Task.objects.create(
+            name="task for tests",
+            status=Status.objects.create(
+                name="status for tests"
+            ),
+            author=User.objects.get(username='Jonny'),
+            description="test description",
+            executor=None,
+        )
+        
+        # входим в приложение под пользователем.
+        self.client.login(username="Jonny", password="password123")  # NOSONAR
     
-#     def test_statuses_list(self):
-#         response = self.client.get(reverse("statuses:statuses"))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertContains(
-#             response,
-#             "status for tests")
+    def test_task_list(self):
+        response = self.client.get(reverse("tasks:tasks"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            "task for tests")
     
-#     def test_status_create(self):
-#         create_url = reverse(
-#             "statuses:create",
-#         )
-#         list_url = reverse("statuses:statuses")
+    def test_status_create(self):
+        create_url = reverse("tasks:create")
+        list_url = reverse("tasks:tasks")
+        status = Status.objects.get(name="status for tests")
 
-#         self.client.post(
-#             create_url,
-#             data={
-#                 "name": "cteate test status"
-#             }
-#         )
-#         response = self.client.get(list_url)
+        self.client.post(
+            create_url,
+            data={
+                "name": "my task for tests",
+                "status": status.id,
+                "author": self.user.pk,
+                "description": "test description",
+                "executor": "",
+            }
+        )
+        response = self.client.get(list_url)
         
-#         self.assertContains(response, "cteate test status")
-#         self.assertContains(response, "status for tests")
+        self.assertContains(response, "my task for tests")
+        self.assertContains(response, "task for tests")
 
-#     def test_status_update(self):
-#         update_url = reverse(
-#             "statuses:update",
-#             kwargs={"pk": self.user.pk}
-#         )
-#         list_url = reverse("statuses:statuses")
+    def test_task_update(self):
+        update_url = reverse(
+            "tasks:update",
+            kwargs={"pk": self.task.pk}
+        )
 
-#         self.client.post(
-#             update_url,
-#             data={
-#                 "name": "update test status"
-#             }
-#         )
-#         response = self.client.get(list_url)
-        
-#         self.assertContains(response, "update test status")
-#         self.assertNotContains(response, "status for tests")
-        
-#     def test_status_delete(self):
-#         delete_url = reverse(
-#             "statuses:delete",
-#             kwargs={"pk": self.user.pk}
-#         )
-#         list_url = reverse("statuses:statuses")
+        list_url = reverse("tasks:tasks")
 
-#         self.client.post(
-#             delete_url,
-#         )
-#         response = self.client.get(list_url)
+        self.client.post(
+            update_url,
+            data={
+                "name": "update tasks test",
+                "status": self.task.status.pk,
+                "author": self.user.pk,
+                "description": "test description",
+                "executor": "",
+            }
+        )
+        response = self.client.get(list_url)
         
-#         self.assertNotContains(response, "status for tests")
+        self.assertContains(response, "update tasks test")
+        self.assertNotContains(response, "task for tests")
+        
+    def test_status_delete(self):
+        delete_url = reverse(
+            "tasks:delete",
+            kwargs={"pk": self.user.pk}
+        )
+        list_url = reverse("tasks:tasks")
+
+        self.client.post(
+            delete_url,
+        )
+        response = self.client.get(list_url)
+        
+        self.assertNotContains(response, "task for tests")
     
 
         
