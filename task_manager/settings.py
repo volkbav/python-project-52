070bff092b2324449a9c19ef988eab64beba3ec6
alex <berezov86@gmail.com>
2026-03-03 17:37:38 +0300

    add compose for productions

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -177,7 +177,7 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # Default primary key field type
