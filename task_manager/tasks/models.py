7cea7c84ff4c3d8682b0064a271448cac36f065d
alex <berezov86@gmail.com>
2026-03-23 11:08:29 +0300

    fix #43 : apdate models tasks and projects

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
+from task_manager.projects.models import Project
 
 
 # Create your models here.
@@ -27,6 +28,14 @@ class Task(models.Model):
         blank=True,
         on_delete=models.SET_NULL,
         related_name='tasks_executor',
+    )
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='tasks_projects',
     )
     
     status = models.ForeignKey(
