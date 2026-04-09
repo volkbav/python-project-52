648cf6595ca63068d80b8266d29eaef641860d88
alex <berezov86@gmail.com>
2026-04-09 16:40:04 +0300

    fix: change updated_at field to auto_now for correct timestamping

--- task_manager/projects/models.py
+++ task_manager/projects/models.py
@@ -65,7 +65,7 @@ class Project(models.Model):
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created_at']
